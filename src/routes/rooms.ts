a98ca82338dbd147c03d64aced4c57fd4720334e
Brandom Rodriguez <brandomrobor@tuta.io>
2020-11-20 14:52:02 -0700

    Find method now skips objects array Also added try-catch block for any error that can occur

--- src/routes/rooms.ts
+++ src/routes/rooms.ts
@@ -9,7 +9,12 @@ roomsRoutes.get("/rooms", async (req, res) => {
     const skip = req.skip as number
     const limit = parseInt(req.query.limit as string)
 
-    res.json(await roomModel.find().skip(skip).limit(limit))
+    try {
+        res.json(await roomModel.find({}, { objects: 0, __v: 0 }).skip(skip).limit(limit))
+    } catch (e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
 })
 
 export default roomsRoutes
