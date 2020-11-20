a6c9d4578812d4ccbfc7923f0f79e43f35ef7324
Brandom Rodriguez <brandomrobor@tuta.io>
2020-11-20 14:18:05 -0700

    Added database connection builder

---
+++ src/helpers/connectionBuilder.ts
@@ -0,0 +1,9 @@
+import { createConnection } from "mongoose"
+
+export const connection = createConnection(
+    process.env.DB_URL as string,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
